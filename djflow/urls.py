8c370735bc798bcf67684985e639304e0f2138ad
DinoRu <diarra.msa@gmail.com>
2022-04-27 15:51:00 +0300

    Final project

--- djflow/urls.py
+++ djflow/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.conf.urls.static import static
 from django.urls import path, include
+from blog2 import views
 from blog2.sitemaps import PostSitemap
 
 
@@ -12,13 +13,16 @@ sitemaps = {
 
 urlpatterns = [
     path('djflow/', admin.site.urls),
+    path('', views.post_list, name='post_list'),
     path('blog/', include('blog.urls', namespace='blog')),
     path('blog2/', include('blog2.urls', namespace='blog2')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
-     name='django.contrib.sitemaps.views.sitemap') 
+         name='django.contrib.sitemaps.views.sitemap')
 ]
 
 if settings.DEBUG:
-    urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
